cd1d54c1f15e942da0e74d85738bf160c8cedde6
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-07-26 21:11:45 +0300

    Add files via upload

---
+++ chessboardclass.cpp
@@ -0,0 +1,8 @@
+//
+//  chessboardclass.cpp
+//  chess
+//
+//  Created by Peter Augerinos on 26/7/22.
+//
+
+#include "chessboardclass.hpp"
---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Peter Augerinos on 26/7/22.
+//
+
+#include <iostream>
+#include <iomanip>
+#include "chessboardclass.hpp"
+using namespace std;
+
+int main() {
+    ChessBoardArray a(4,4);
+    cout << a;
+    
+
+    
+    
+    
+    
+    return 0;
+}
