e875be83c2c7f56be8f2a5e075993f99bc474fc3
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-07-31 19:00:51 +0300

    another not so certain commit

--- dlist/containers.cpp
+++ dlist/containers.cpp
@@ -12,50 +12,7 @@ class Container {
         virtual void clear() = 0;
 };
 
-template <typename T>
-class Iterable { 
-    public: 
-        virtual Iterator<T> begin() = 0;
-        virtual Iterator<T> end() = 0;
-};
 
-template <typename T>
-class Iterator { 
-    public: 
-        Iterator (const Iterator &i) : impl(i.impl->clone()) {} ;
-        ~Iterator() { delete impl; }
-        T & operator *() { 
-            return impl->access(); }
-        };
-
-        Iterator & operator ++() { 
-            impl->advance();
-            return *this;
-        };
-
-        Iterator operator ++ (int) { 
-            Iterator result(*this);
-            impl->advance();
-            return result;
-        };
-
-        bool operator == (const Iterator &i) const { return impl->equal(*(i.impl)); };
-        bool operator != (const Iterator &i) const { return !(*this == i); };
-
-        class Impl { 
-            public: 
-                virtual ~Impl() {}
-                virtual Impl *clone() const = 0;
-                virtual T & access() const = 0;
-                virtual void advance() = 0;
-                virtual bool equal(const Impl &i) const = 0;
-        };
-
-        Iterator(Impl *i) : impl(i) {}
-
-    private: 
-        Impl *impl;
-};
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
---
+++ polynomial/polynomial2.hpp
@@ -0,0 +1,99 @@
+#include <__nullptr>
+#include <iostream> 
+#include <list>
+
+using namespace std; 
+
+class Polynomial { 
+    protected: 
+        class Term { 
+            int exponent; 
+            int coefficient;
+            Term *next;
+            Term(int exp, int coeff, Term *n): exponent(exp), coefficient(coeff), next(n) {}
+            friend class Polynomial;
+        };
+
+        Term *the_front, *the_back;
+        int the_size;
+        
+        void copy(const Polynomial &p) { 
+            for (Term *t = the_front; t != nullptr; t = t->next) { 
+                addTerm(t->exponent, t->coefficient);
+            }
+        }
+
+        void purge() { 
+            Term *p = the_front;
+            while (p != nullptr)  {
+                Term *q = p; 
+                p = p->next;
+                delete q;
+            }
+        }
+
+    public:
+        Polynomial() { 
+            the_front = nullptr;
+            the_back = nullptr; 
+            the_size = 0;
+        };
+
+        Polynomial(const Polynomial &p) {
+            the_front = nullptr;
+            the_back = nullptr; 
+            the_size = 0;
+            copy(p);
+        };
+
+        ~Polynomial() { 
+            purge();
+        };
+
+        Polynomial & operator = (const Polynomial &p) { 
+            clear();
+            copy(p);
+            return *this;
+        };
+
+        void addTerm(int exp, int coeff) { 
+            Term *p = new Term(exp, coeff, nullptr);
+            Term *t = the_front; 
+            while (p->exponent < t->exponent) { 
+                t = t->next; 
+            }
+            p->next = t; 
+        };
+
+        double evaluate(double x) const { 
+            Term *t = the_front;
+            while (t->exponent != x) { 
+                t = t->next;
+            }
+            return t->coefficient;
+        };
+
+        int size() { 
+            return the_size;
+        }
+    
+        void clear() { 
+            purge();
+            the_back = nullptr;
+            the_front = nullptr; 
+            the_size = 0;
+        }
+
+        friend Polynomial & operator + (const Polynomial &a, const Polynomial &b) {
+            Polynomial *p = new Polynomial();
+            for (Term *t = p->the_front; t != nullptr; t = t->next) { 
+                p->addTerm(t->exponent, a.evaluate(t->coefficient) + b.evaluate(t->coefficient));
+            }
+            return *p;
+        };
+
+        friend Polynomial & operator * (const Polynomial &a, const Polynomial &b);
+
+        friend ostream & operator << (ostream &out, const Polynomial &p);
+
+};
