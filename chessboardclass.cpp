5a2b6291c0ab679b080a0674feaf126d11a37fcb
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-08-02 18:46:18 +0300

    deleted:    Greedy_Player.hpp deleted:    Player.hpp 	deleted:    Righteous_Player.hpp 	deleted:    Sneaky_Player.hpp 	deleted:    Spartan_Player.hpp deleted:    chessboardclass.cpp 	deleted:    chessboardclass.hpp 	deleted:    containers.cpp 	deleted:    game.cpp 	deleted:    game.hpp 	deleted:    lecture4.xcodeproj.zip 	deleted:    main.cpp 	deleted:    move.cpp 	deleted:    move.hpp 	deleted:    objects.cpp 	deleted:    objects.hpp 	deleted:    rationalclass.cpp 	deleted:    rationalclass.hpp 	deleted:    specker.xcodeproj.zip 	deleted:    stackimpl.cpp 	deleted:    stackimpl.hpp 	deleted:    state.cpp 	deleted:    state.hpp

--- chessboardclass.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  chessboardclass.cpp
-//  chess
-//
-//  Created by Peter Augerinos on 26/7/22.
-//
-
-#include "chessboardclass.hpp"
--- objects.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  objects.cpp
-//  lecture4
-//
-//  Created by Peter Augerinos on 25/7/22.
-//
-
-#include "objects.hpp"
