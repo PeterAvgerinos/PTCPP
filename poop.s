e3794b301e977869b791cc67fa353df78b80bc1d
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-22 14:21:44 +0300

    deleted:    poop.cpp deleted:    poop.s 	modified:   Stackdemo/stackdemo (modified content, untracked content) 	modified:   specker (modified content, untracked content)

--- poop.s
+++
@@ -1,5 +0,0 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 12, 0	sdk_version 12, 3
-	.globl	_array                          ; @array
-.zerofill __DATA,__common,_array,8,3
-.subsections_via_symbols
